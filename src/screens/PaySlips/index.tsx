398d869d54d71f330f809c19bc7bd4c0494d5154
Justin <justinjoyn@gmail.com>
2024-02-13 18:21:38 +0000

    Integrated payslip component

--- src/screens/PaySlips/index.tsx
+++ src/screens/PaySlips/index.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import { Container } from '@mui/system';
+import { useState } from 'react';
+
+import CustomAppBar from '../../components/CustomAppBar';
+import PayslipTable from '../../components/PayslipTable';
+import { mockPayslips } from '../../assets/data/payslips';
+import { Payslip } from '../../types/common';
 
 export default function PaySlips() {
-    return <div>PaySlips</div>;
+    const [payslips, setPayslips] = useState<Payslip[]>(mockPayslips);
+
+    return (
+        <>
+            <CustomAppBar />
+            <Container>
+                <PayslipTable payslips={payslips} />
+            </Container>
+        </>
+    );
 }
